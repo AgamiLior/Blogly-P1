8f8ea0966c679247f9198baf3c1fb4a577854ede
root <root@DESKTOP-1ODHT3M.localdomain>
2021-10-09 14:42:53 -0400

    part two

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 
@@ -23,20 +23,20 @@ def home():
 
 @app.route('/users')
 def users_index():
-    """Show a page with info on all users"""
+    """Show list of users"""
 
     users = User.query.order_by(User.last_name, User.first_name).all()
     return render_template('users/index.html', users=users)
 
 @app.route('/users/new', methods=["GET"])
 def users_new_form():
-    """Show a form to create a new user"""
+    """Create new user form"""
 
     return render_template('users/new.html')
 
 @app.route("/users/new", methods=["POST"])
 def users_new():
-    """Handle form submission for creating a new user"""
+    """Form to handle all of user's information"""
 
     new_user = User(
         first_name=request.form['first_name'],
@@ -51,7 +51,7 @@ def users_new():
 
 @app.route('/users/<int:user_id>')
 def users_show(user_id):
-    """Show a page with info on a specific user"""
+    """Shows user's page"""
 
     user = User.query.get_or_404(user_id)
     return render_template('users/userpage.html', user=user)
@@ -59,7 +59,7 @@ def users_show(user_id):
 
 @app.route('/users/<int:user_id>/edit')
 def users_edit(user_id):
-    """Show a form to edit an existing user"""
+    """Edit user's page"""
 
     user = User.query.get_or_404(user_id)
     return render_template('users/edit.html', user=user)
@@ -67,7 +67,7 @@ def users_edit(user_id):
 
 @app.route('/users/<int:user_id>/edit', methods=["POST"])
 def users_update(user_id):
-    """Handle form submission for updating an existing user"""
+    """Form to handle user edit page"""
 
     user = User.query.get_or_404(user_id)
     user.first_name = request.form['first_name']
@@ -82,10 +82,74 @@ def users_update(user_id):
 
 @app.route('/users/<int:user_id>/delete', methods=["POST"])
 def users_destroy(user_id):
-    """Handle form submission for deleting an existing user"""
+    """delete user"""
 
     user = User.query.get_or_404(user_id)
     db.session.delete(user)
     db.session.commit()
 
     return redirect("/users")
+
+
+@app.route('/users/<int:user_id>/posts/new')
+def posts_new_form(user_id):
+    """Create new post for user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('posts/new.html', user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])
+def posts_new(user_id):
+    """Form to handle the user's post"""
+
+    user = User.query.get_or_404(user_id)
+    new_post = Post(title=request.form['title'],
+                    content=request.form['content'],
+                    user=user)
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(f"/users/{user_id}")
+
+
+@app.route('/posts/<int:post_id>')
+def posts_show(post_id):
+    """Show all posts for user"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/show.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def posts_edit(post_id):
+    """Edit user's specific post"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/edit_post.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def posts_update(post_id):
+    """Form to handle post edit"""
+
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f"/users/{post.user_id}")
+
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def posts_destroy(post_id):
+    """Delete specific post"""
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(f"/users/{post.user_id}")
